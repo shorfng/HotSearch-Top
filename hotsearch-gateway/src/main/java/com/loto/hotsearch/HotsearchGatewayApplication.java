e81496866798c2def467c25869bdf4d4cac9c536
蓝田_Loto <shorfng@126.com>
2023-01-12 01:17:23 +0800

    hotsearch-网关（暂时有问题，只做初步搭建）

---
+++ hotsearch-gateway/src/main/java/com/loto/hotsearch/HotsearchGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.loto.hotsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * PageName：HotsearchGatewayApplication.java<p>
+ * Date：2023-01-12 0:17<p>
+ * Function：hotsearch-网关
+ *
+ * @author 蓝田_Loto
+ */
+
+@SpringBootApplication
+public class HotsearchGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HotsearchGatewayApplication.class, args);
+    }
+}
